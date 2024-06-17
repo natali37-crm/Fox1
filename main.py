694bc0db799cada761675a89507b3303683f59bb
natali37-crm <natal.filimonova2016@yandex.ru>
2024-06-17 23:25:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+name = 'Nataliya'
+print("Hello " + name)
+print(name[0])
+print(name[-7])
+print(name[4:])
+print(name[::-1])
+a = "Топинамбур"
+print(a[0])
+print(a[9:])
+print(a[::-1])
+print(a[1:10:2])
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[0:9:2])
+print(example[1:10:2])
+
+
+
+
